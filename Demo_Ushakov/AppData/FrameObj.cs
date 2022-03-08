537bebea1b453b26f6649dd0cf0ae5b0a157ad05
aerodes <aerodes.us@gmail.com>
2022-03-08 10:45:48 +0300

    My first commit

---
+++ Demo_Ushakov/AppData/FrameObj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Demo_Ushakov.AppData
+{
+    class FrameObj
+    {
+        public static Frame frameMain;
+    }
+}
---
+++ Demo_Ushakov/Pages/PageButton1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Demo_Ushakov.AppData;
+using Demo_Ushakov.Pages;
+
+namespace Demo_Ushakov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageButton1.xaml
+    /// </summary>
+    public partial class PageButton1 : Page
+    {
+        public PageButton1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObj.frameMain.Navigate(new PageButton2());
+        }
+    }
+}
