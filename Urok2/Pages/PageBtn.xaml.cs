c636aac57ee7d7c3b04ab8201b4b65cc663e8a57
aerodes <aerodes.us@gmail.com>
2022-03-21 09:33:58 +0300

    My 2 lesson

---
+++ Urok2/Pages/PageBtn.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Urok2.Pages;
+using Urok2.AppFile;
+
+namespace Urok2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageBtn.xaml
+    /// </summary>
+    public partial class PageBtn : Page
+    {
+        public PageBtn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCVT_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObj.frame.Navigate(new PageCVT());
+        }
+
+        private void btnLVT_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObj.frame.Navigate(new PageLU());
+        }
+    }
+}
---
+++ Urok2/Pages/PageCVT.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Func;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Urok2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageCVT.xaml
+    /// </summary>
+    public partial class PageCVT : Page
+    {
+        public PageCVT()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            double a, b, c;
+            if (double.TryParse(A.Text, out a) && double.TryParse(B.Text, out b) && double.TryParse(C.Text, out c))
+            {
+                ClassCVT.KV(a, b, c, out string messege);
+                if (!string.IsNullOrEmpty(messege)) _ = txbRes.Text;
+            }
+        }
+    }
+}
