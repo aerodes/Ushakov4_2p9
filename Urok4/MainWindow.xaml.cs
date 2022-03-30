2d9889c1ac360f2ec9e8fdf83affa9e03a9b957c
aerodes <aerodes.us@gmail.com>
2022-03-30 09:35:22 +0300

    Urok 4 or 5

---
+++ Urok4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Urok4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string log = "admin";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtlog.Text == log & txtpass.Text == log)
+            {
+                WindowLegent windowLegent = new WindowLegent();
+                MainWindow mainWindow = new MainWindow();
+                windowLegent.Show();
+                mainWindow.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неправильные данные", "Ошибко входа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
